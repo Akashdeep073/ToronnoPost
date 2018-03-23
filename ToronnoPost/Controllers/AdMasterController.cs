1cc2294a2cbf9daad4aba7c8a9d2efbf40253486
Akashongithub <32825438+Akashongithub@users.noreply.github.com>
2018-03-22 23:32:06 -0400

    Admin can now edit postede ads!

---
+++ ToronnoPost/Controllers/AdMasterController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class AdMasterController : Controller
+    {
+        // GET: AdMaster
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index1()
+        {
+            List<ad_master> model = new List<ad_master>();
+            using (AdMasterEntities dc = new AdMasterEntities())
+            {
+                model = dc.ad_master.ToList();
+            }
+            return View(model);     
+        }
+
+
+        [HttpPost]
+
+        public ActionResult Index1(List<ad_master> list)
+
+        {
+            if (ModelState.IsValid)
+            {
+                using (AdMasterEntities dc = new AdMasterEntities())
+                {
+                    foreach (var i in list)
+                    {
+                        var c = dc.ad_master.Where(a => a.adid.Equals(i.adid)).FirstOrDefault();
+                        if (c != null)
+                        {
+                            c.address = i.address;
+                            c.ad_type = i.ad_type;                            
+                            c.description = i.description;                            
+                            c.location = i.location;                            
+                            c.price = i.price;
+                            c.title = i.title;                            
+                        }
+                    }
+                    dc.SaveChanges();
+                }
+                ViewBag.Message = "Successfully Updated.";
+                return View( list);
+            }
+            else
+            {
+                ViewBag.Message = "Failed ! Please try again.";
+                return View(list);
+            }
+
+        }
+    }
+}
--- ToronnoPost/Controllers/UserTableController.cs
+++ ToronnoPost/Controllers/UserTableController.cs
@@ -28,54 +28,98 @@ namespace WebShop.Controllers
 
         }
 
-
+        [HttpGet]
         public ActionResult Index1()
         {
+            List<AspNetUser> model = new List<AspNetUser>();
+            using (webshopEntities dc = new webshopEntities())
+            {
+                model = dc.AspNetUsers.ToList();
+            }
+            return View(model);
 
-            webshopEntities DbCompany = new webshopEntities();
 
-            var data = from item in DbCompany.AspNetUsers
 
-                           //where item.Company == "TCS"
+            //webshopEntities DbCompany = new webshopEntities();
 
-                       orderby item.Id
+            //var data = from item in DbCompany.AspNetUsers
 
-                       select item;
+            //               //where item.Company == "TCS"
 
-            return View(data.ToList());
+            //           //orderby item.Id
+
+            //           select item;
+
+            //return View(data.ToList());
 
         }
 
 
         [HttpPost]
 
-        public ActionResult Index1(List<AspNetUser> employees)
+        public ActionResult Index1(List<AspNetUser> list)
 
         {
-
-            webshopEntities DbCompany = new webshopEntities();
-
-
-
-            foreach (AspNetUser Emp in employees)
-
+            if (ModelState.IsValid)
             {
-
-                AspNetUser Existed_Emp = DbCompany.AspNetUsers.Find(Emp.Id);
-
-                Existed_Emp.Id = Emp.Id;
-
-                Existed_Emp.Email = Emp.Email;
-                Existed_Emp.PasswordHash = Emp.PasswordHash;
-                Existed_Emp.PhoneNumber = Emp.PhoneNumber;
-
+                using (webshopEntities dc = new webshopEntities())
+                {
+                    foreach (var i in list)
+                    {
+                        var c = dc.AspNetUsers.Where(a => a.Id.Equals(i.Id)).FirstOrDefault();
+                        if (c != null)
+                        {
+                            c.Email = i.Email;
+                            c.PasswordHash = i.PasswordHash;
+                            c.PhoneNumber = i.PhoneNumber;
+                            c.UserName  = i.UserName;
+                            //    Existed_Emp.PasswordHash = Emp.PasswordHash;
+                            //    Existed_Emp.PhoneNumber = Emp.PhoneNumber;
+                        }
+                    }
+                    dc.SaveChanges();
+                }
+                ViewBag.Message = "Successfully Updated.";
+                return View(list);
+            }
+            else
+            {
+                ViewBag.Message = "Failed ! Please try again.";
+                return View(list);
             }
 
 
 
-            DbCompany.SaveChanges();
 
-            return View();
+
+
+
+
+
+
+            //webshopEntities DbCompany = new webshopEntities();
+
+
+
+            //foreach (AspNetUser Emp in aspnetuser)
+
+            //{
+
+            //    AspNetUser Existed_Emp = DbCompany.AspNetUsers.Find(Emp.Id);
+
+            //    Existed_Emp.Id = Emp.Id;
+
+            //    Existed_Emp.Email = Emp.Email;
+            //    Existed_Emp.PasswordHash = Emp.PasswordHash;
+            //    Existed_Emp.PhoneNumber = Emp.PhoneNumber;
+
+            //}
+
+
+
+            //DbCompany.SaveChanges();
+
+            //return View();
 
         }
     }
